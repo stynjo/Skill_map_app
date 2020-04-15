1f8dfefdc90dd1cb1611f3d3b357e221951e385b
k.kei <acidjannedoor@yahoo.co.jp>
2020-04-15 08:19:01 +0000

    indexコントローラ view追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
+  
  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
@@ -11,4 +10,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
  root 'static_pages#top'
+ 
+ resources :users do
+  end
 end
